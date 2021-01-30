f0fd3d38922cd2c002a3a0498a190d69ee0b2c15
AlexKovaliov <lawsansana@gmail.com>
2021-01-30 19:39:17 +0300

    created Header block added styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import s from './Header.module.css'
+import harleyBg from '../../img/header-bg-1.jpg'
+import logoHW from '../../img/logo.svg'
 
 
 type headerDataType = { title: string, way: string } []
 
 
 export function Header(props: { links: headerDataType }) {
+
+    const backImg = {
+        backgroundImage: `url(${harleyBg})`
+    }
+
+    const logo = {
+        backgroundImage: `url(${logoHW})`
+    }
+
     return (
-        <header className={s.header}>
+        <header className={s.header} style={backImg}>
             <div className={s.container}>
                 <div className={s.row}>
-                    <div className={s.logo}>
-                        <img src="" alt=""/>
+                    <div className={s.logo} style={logo}>
                     </div>
-                    <nav>
+                    <nav className={s.nav}>
                         {props.links.map((value) => {
                             return (
-                                <a href={value.way}>{value.title}</a>
+                                <a className={s.navLink} href={value.way}>{value.title}</a>
                             )
                         })}
                     </nav>
