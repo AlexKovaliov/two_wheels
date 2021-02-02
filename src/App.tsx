42aea47dbc0c21472d70d4f31e632016bbbc95e1
AlexKovaliov <lawsansana@gmail.com>
2021-02-02 22:38:28 +0300

    created Product block

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {TopPanel} from "./components/Top_panel/TopPanel";
 import {headerData} from "./components/Header/HeaderData";
 import {Header} from "./components/Header/Header";
 import {Main} from "./components/Main/Main";
+import {Products} from "./components/Products/Products";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <TopPanel/>
             <Header links={headerData}/>
             <Main/>
+            <Products/>
         </div>
     );
 }
---
+++ src/components/Products/Products.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './Products.module.css'
+
+
+export function Products() {
+    return (
+        <div className={s.products}>
+            <div className={s.container}>
+                <div className={s.title}>
+                    Our Product
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
