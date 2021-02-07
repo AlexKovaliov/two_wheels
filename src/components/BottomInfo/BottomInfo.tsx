63e7cd1aed58ac67fc0589e97553848e2f32e3db
AlexKovaliov <lawsansana@gmail.com>
2021-02-07 18:49:51 +0300

    added images

--- src/components/BottomInfo/BottomInfo.tsx
+++ src/components/BottomInfo/BottomInfo.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import s from './BottomInfo.module.css'
 import info5 from "../../img/info/info5.jpg";
+import info6 from "../../img/info/info6.jpg";
 
 
 export function BottomInfo() {
 
     const Info5Img = {
         backgroundImage: `url(${info5})`
+    }
+
+    const Info6Img = {
+        backgroundImage: `url(${info6})`
     }
 
     return (
@@ -19,6 +24,14 @@ export function BottomInfo() {
                         <a className={s.content} href="#">
                             <span className={s.title}>for the purchase of any motorcycle for the customers of the MotoHarley motorcycle school</span>
                             <span className={s.discount}><span>up to</span>30%</span>
+                        </a>
+                    </div>
+
+                    <div className={s.large}>
+                        <img className={s.img} style={Info6Img} alt="Motorcycle Harley Davidson"/>
+                        <a className={s.content} href="#">
+                            <span className={s.title}>Discounts for regular and wholesale customers from our motorcycle house.</span>
+                            <span className={s.discount}><span>up to</span>15-50%</span>
                         </a>
                     </div>
 
